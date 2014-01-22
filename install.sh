9d20b739289c14fc6ff794eaa223cd4abefa7a28
Kevin Most <kevin.most.93@gmail.com>
2014-01-22 11:13:08 -0500

    No longer requires root
    Script now creates a directory called “.adb-fastboot” in the user’s
    home directory and adds this directory to your .bash_profile
    
    Note that this will only install ADB and Fastboot for the user who
    invokes the application, as it does not have privileges to install
    somewhere that all users can access.

--- install.sh
+++ install.sh
@@ -13,38 +13,39 @@
 
 #!/bin/bash
 
-# get sudo
-
 echo "[INFO] Nexus Tools Installer 1.0"
-echo "[INFO] Please enter sudo password for adb/fastboot install"
-sudo echo "[ OK ] Sudo access granted."
+echo "[INFO] Please note that because this script does not require root access, it installs into the user's Home folder. Therefore it may not be available for other users of this machine."
 
 # check operating system
 
 if [ "$(uname)" == "Darwin" ]; then # Mac OS X
-	cd /usr/bin/
+	mkdir ~/.adb-fastboot/ && cd ~/.adb-fastboot/
 	echo "[INFO] Downloading ADB for Mac OS X..."
-    sudo curl -s -o adb "http://github.com/corbindavenport/nexus-tools/blob/master/macosx/adb?raw=true" -LOk
+    curl -s -o adb "http://github.com/corbindavenport/nexus-tools/blob/master/macosx/adb?raw=true" -LOk
     echo "[ OK ] ADB finished downloading."
     echo "[INFO] Downloading Fastboot for Mac OS X..."
-    sudo curl -s -o fastboot "http://github.com/corbindavenport/nexus-tools/blob/master/macosx/fastboot?raw=true" -LOk
+    curl -s -o fastboot "http://github.com/corbindavenport/nexus-tools/blob/master/macosx/fastboot?raw=true" -LOk
     echo "[ OK ] Fastboot finished downloading."
     echo "[INFO] Making ADB and Fastboot executable..."
-    sudo chmod +x ./adb
-    sudo chmod +x ./fastboot
+    chmod +x ./adb
+    chmod +x ./fastboot
+    echo "[INFO] Adding ADB and Fastboot to path..."
+    echo export PATH=~/.adb-fastboot/:$PATH >> ~/.bash_profile
     echo "[ OK ] Done!"
     echo "[INFO] Type adb or fastboot to run."
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then # Linux
-    cd /usr/bin/
+    mkdir ~/.adb-fastboot/ && cd ~/.adb-fastboot/
 	echo "[INFO] Downloading ADB for Linux..."
-    sudo curl -s -o adb "http://github.com/corbindavenport/nexus-tools/blob/master/linux/adb?raw=true" -LOk
+    curl -s -o adb "http://github.com/corbindavenport/nexus-tools/blob/master/linux/adb?raw=true" -LOk
     echo "[ OK ] ADB finished downloading."
     echo "[INFO] Downloading Fastboot for Linux..."
-    sudo curl -s -o fastboot "http://github.com/corbindavenport/nexus-tools/blob/master/linux/fastboot?raw=true" -LOk
+    curl -s -o fastboot "http://github.com/corbindavenport/nexus-tools/blob/master/linux/fastboot?raw=true" -LOk
     echo "[ OK ] Fastboot finished downloading."
     echo "[INFO] Making ADB and Fastboot executable..."
-    sudo chmod +x ./adb
-    sudo chmod +x ./fastboot
+    chmod +x ./adb
+    chmod +x ./fastboot
+    echo "[INFO] Adding ADB and Fastboot to path..."
+    echo export PATH=~/.adb-fastboot/:$PATH >> ~/.bash_profile
     echo "[ OK ] Done!"
     echo "[INFO] Type adb or fastboot to run."
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then # Cygwin on Windows
